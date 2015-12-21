547444439e99005d9cd96502dced06607c1ef384
Oleg <shinkarenko_oleg@mail.ua>
2015-12-21 18:55:54 +0200

    Created client. Started server.

--- Test/Assets/Scripts/ParseScr.cs
+++ Test/Assets/Scripts/ParseScr.cs
@@ -2,28 +2,40 @@
 using System.Collections;
 using Parse;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ParseScr: MonoBehaviour {
 
-	private string userId;
-	private string f;
-
-	public void InpField2(string inputNumber)
-	{
-		userId = inputNumber;
-
-	}
-
-	public void Update (){
-		Debug.Log(userId);
-	}
-
-
 	public void VersionButton1()
 	{
-		var version1 = new ParseObject ("Version1");
-		version1 ["userId"] = 7;
-		version1.SaveAsync().ContinueWith(t=> {Debug.Log("Finished! ");});
+		GameObject inputFieldGo = GameObject.Find("InputFieldId");
+		InputField inputFieldCo = inputFieldGo.GetComponent<InputField>();
+	
+	    var version1 = new ParseObject ("Version1");
+		version1 ["userId"] = inputFieldCo.text;
+		version1.SaveAsync(); 
+
+		//SceneManager.LoadScene("Second");
+	}
+	public void VersionButton2()
+	{
+		GameObject inputFieldGo = GameObject.Find("InputFieldId");
+		InputField inputFieldCo = inputFieldGo.GetComponent<InputField>();
+
+		var version2 = new ParseObject ("Version2");
+		version2 ["userId"] = inputFieldCo.text;
+		version2.SaveAsync(); 
+
+		//SceneManager.LoadScene("Second");
+	}
+	public void VersionButton3()
+	{
+		GameObject inputFieldGo = GameObject.Find("InputFieldId");
+		InputField inputFieldCo = inputFieldGo.GetComponent<InputField>();
+
+		var version3 = new ParseObject ("Version3");
+		version3 ["userId"] = inputFieldCo.text;
+		version3.SaveAsync(); 
 
 		//SceneManager.LoadScene("Second");
 	}
