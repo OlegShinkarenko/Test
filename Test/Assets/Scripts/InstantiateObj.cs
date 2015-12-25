3dd16dc526f29c5b2dac3ebce1683d5ef8e0393c
Oleg <shinkarenko_oleg@mail.ua>
2015-12-25 20:18:59 +0200

    Server alpha.

--- Test/Assets/Scripts/InstantiateObj.cs
+++ Test/Assets/Scripts/InstantiateObj.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Parse;
+using System.Threading.Tasks;
 
 public class InstantiateObj : MonoBehaviour {
-
+	
 	public GameObject box1;
 	public GameObject box2;
 	public GameObject box3;
@@ -15,27 +16,102 @@ public class InstantiateObj : MonoBehaviour {
 	public GameObject input4;
 
 	public GameObject canvas;
+	private bool A{ get; set;}
+	private bool B{ get; set;}
+	private bool X{ get; set;}
+	private bool Y{ get; set;}
+	private GameState _gameState;
 
-	public void Start () {
-		
+	private string s1 = "Version1";
+	private string s2 = "Version2";
+	private string s3 = "Version3";
 
-
-
-			Instantiate (box1, new Vector3 (-6, 3, 0), Quaternion.identity);
-			Instantiate (box2, new Vector3 (-6, 1, 0), Quaternion.identity);
-			Instantiate (box3, new Vector3 (-6, -1, 0), Quaternion.identity);
-			Instantiate (box4, new Vector3 (-6, -3, 0), Quaternion.identity);
-
-		GameObject a = Instantiate(input1) as GameObject;
-		a.transform.SetParent(canvas.transform, false);
-		GameObject b = Instantiate(input2) as GameObject;
-		b.transform.SetParent(canvas.transform, false);
-		GameObject x = Instantiate(input3) as GameObject;
-		x.transform.SetParent(canvas.transform, false);
-		GameObject y = Instantiate(input4) as GameObject;
-		y.transform.SetParent(canvas.transform, false);
+	private enum GameState
+	{
+		First,
+		Second,
 	}
 
+	public void Start(){
+		GoToState (GameState.First);
+		StartCoroutine(Example());
+		}
 
+	public IEnumerator Example() {
+		yield return new WaitForSeconds(1);
+		GoToState (GameState.Second);
+	}
 
+	private void GoToState(GameState state)
+	{
+		switch (state) {
+
+		case GameState.First:
+			Queri ();
+			break;
+
+		case GameState.Second:
+			
+			Ins (A, B, X, Y);
+			break;
+		}
+	}
+
+	public void Queri(){
+		if (ParseScr.s1 == s1) {
+			ParseQuery<ParseObject> queri = ParseObject.GetQuery (ParseScr.s1);
+			queri.GetAsync ("AAnuURQ4iz").ContinueWith (t => {
+				ParseObject versiun1 = t.Result;
+				A = versiun1.Get<bool> ("visibilityA");
+				B = versiun1.Get<bool> ("visibilityB");
+				X = versiun1.Get<bool> ("visibilityX");
+				Y = versiun1.Get<bool> ("visibilityY");
+
+			} 
+			);
+		} else if (ParseScr.s2 == s2) {
+			ParseQuery<ParseObject> queri2 = ParseObject.GetQuery (ParseScr.s2);
+			queri2.GetAsync ("R24mG8jWzC").ContinueWith (t => {
+				ParseObject versiun2 = t.Result;
+				A = versiun2.Get<bool> ("visibilityA");
+				B = versiun2.Get<bool> ("visibilityB");
+				X = versiun2.Get<bool> ("visibilityX");
+				Y = versiun2.Get<bool> ("visibilityY");
+			} 
+			);
+		} else if (ParseScr.s3 == s3) {
+			ParseQuery<ParseObject> queri3 = ParseObject.GetQuery (ParseScr.s3);
+			queri3.GetAsync ("eG8l6OY2q1").ContinueWith (t => {
+				ParseObject versiun3 = t.Result;
+				A = versiun3.Get<bool> ("visibilityA");
+				B = versiun3.Get<bool> ("visibilityB");
+				X = versiun3.Get<bool> ("visibilityX");
+				Y = versiun3.Get<bool> ("visibilityY");
+			} 
+			);
+		}
+
+	}
+	public void Ins ( bool visA, bool visB, bool visX, bool visY) {
+		if (visA) {
+			Instantiate (box1, new Vector3 (-6, 3, 0), Quaternion.identity);
+			GameObject a = Instantiate(input1) as GameObject;
+			a.transform.SetParent(canvas.transform, false);
+		}
+		if (visB) {
+			Instantiate (box2, new Vector3 (-6, 1, 0), Quaternion.identity);
+			GameObject b = Instantiate(input2) as GameObject;
+			b.transform.SetParent(canvas.transform, false);
+		}
+		if (visX) {
+			Instantiate (box3, new Vector3 (-6, -1, 0), Quaternion.identity);
+			GameObject x = Instantiate(input3) as GameObject;
+			x.transform.SetParent(canvas.transform, false);
+		}
+		if (visY) {
+			Instantiate (box4, new Vector3 (-6, -3, 0), Quaternion.identity);
+			GameObject y = Instantiate(input4) as GameObject;
+			y.transform.SetParent(canvas.transform, false);
+		}
+	}
 }
--- Test/Assets/Scripts/ParseScr.cs
+++ Test/Assets/Scripts/ParseScr.cs
@@ -4,63 +4,63 @@ using Parse;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class ParseScr: MonoBehaviour {
 
- 
-	public string a;
+	public static string s1, s2, s3;
+
 	public void VersionButton1()
-	{
+	{	
+		s1 = "Version1";
 		GameObject inputFieldGo = GameObject.Find("InputFieldId");
 		InputField inputFieldCo = inputFieldGo.GetComponent<InputField>();
 
-	    /*var version1 = new ParseObject ("Version1");
+	    var version1 = new ParseObject ("Version1");
 		version1 ["userId"] = inputFieldCo.text;
 		version1 ["visibilityA"] = true;
 		version1 ["visibilityB"] = true;
 		version1 ["visibilityX"] = false;
 		version1 ["visibilityY"] = false;
-		version1 ["visibilityBoxA"] = true;
-		version1 ["visibilityBoxB"] = true;
-		version1 ["visibilityBoxX"] = false;
-		version1 ["visibilityBoxY"] = false;*/
 
-		ParseQuery<ParseObject> queri = ParseObject.GetQuery ("Version1");
-		queri.GetAsync ("833J3yyBnu").ContinueWith(t => 
-			{
-				ParseObject versiun1 = t.Result;
-				string s = versiun1.Get<string>("userId");
-				a = s;
-			} 
-		);
-		Debug.Log (a);
-		//version1.SaveAsync ();
-
-
+		 version1.SaveAsync ();
+	
 		SceneManager.LoadScene ("Second");
 	}
 
-	/*public void VersionButton2()
+	public void VersionButton2()
 	{
+		s2 = "Version2";
 		GameObject inputFieldGo = GameObject.Find("InputFieldId");
 		InputField inputFieldCo = inputFieldGo.GetComponent<InputField>();
 
 		var version2 = new ParseObject ("Version2");
 		version2 ["userId"] = inputFieldCo.text;
-		version2.SaveAsync(); 
+		version2 ["visibilityA"] = true;
+		version2 ["visibilityB"] = true;
+		version2 ["visibilityX"] = true;
+		version2 ["visibilityY"] = false;
 
-		//SceneManager.LoadScene("Second");
+		version2.SaveAsync ();
+
+		SceneManager.LoadScene("Second");
 	}
 	public void VersionButton3()
 	{
+		s3 = "Version3";
 		GameObject inputFieldGo = GameObject.Find("InputFieldId");
 		InputField inputFieldCo = inputFieldGo.GetComponent<InputField>();
 
 		var version3 = new ParseObject ("Version3");
 		version3 ["userId"] = inputFieldCo.text;
+		version3 ["visibilityA"] = false;
+		version3 ["visibilityB"] = false;
+		version3 ["visibilityX"] = true;
+		version3 ["visibilityY"] = true;
+
 		version3.SaveAsync(); 
 
-		//SceneManager.LoadScene("Second");
-	}*/
+		SceneManager.LoadScene("Second");
+	}
 		
 
 		
